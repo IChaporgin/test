3fbb0f6177b8ceaca8f220014ed9f4156cddec1a
Igor Chaporgin <fellclub@yandex.ru>
2023-03-22 11:45:00 +0300

    v 1.0

---
+++ Lesson/Lesson/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Lesson
+//
+//  Created by Igor Chaporgin on 21.03.2023.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
